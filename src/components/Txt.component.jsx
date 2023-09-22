ee490367fe85cd69c0cccc540bcadaf528d3201c
loopy-lim <loopy@jnu.ac.kr>
2023-09-23 01:27:56 +0900

    feat: init project file
    - add capacitor
    - add axios
    - add react-query
    - add react-router-dom
    - add classnames
    - add jotai
    - add react
    - add vite
    - add tailwind
    - add prettier
    - add eslint
    - setting router
    - setting init capacitor files
    - add example jsx files with JSDocs
    - init components folder
    - ini pages folder

---
+++ src/components/Txt.component.jsx
@@ -0,0 +1,28 @@
+import classnames from "classnames";
+
+const typographyType = {
+  h1: "text-4xl font-bold",
+  h2: "text-3xl font-bold",
+  h3: "text-2xl font-bold",
+  h4: "text-xl font-bold",
+  h5: "text-lg font-bold",
+  h6: "text-base font-bold",
+  p: "text-base",
+};
+
+const colorsType = {
+  black: "text-black",
+};
+
+/** @param {{typography: keyof typeof typographyType, colors: keyof typeof colorsType, children: import("react").ReactNode}}*/
+const Txt = ({ typography = "p", colors = "black", children }) => {
+  return (
+    <span
+      className={classnames(typographyType[typography], colorsType[colors])}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default Txt;
---
+++ src/pages/home.page.jsx
@@ -0,0 +1,7 @@
+import Title from "../components/Title.component";
+
+const HomePage = () => {
+  return <Title>hello world</Title>;
+};
+
+export default HomePage;
