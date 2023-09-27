752fecb1c260fb7adca49291e95ad2211ffc9e55
loopy-lim <loopy@jnu.ac.kr>
2023-09-27 14:45:18 +0900

    feat: add br has navigate (this bransh can't used)

--- src/components/common/Br.component.jsx
+++ src/components/common/Br.component.jsx
@@ -1,7 +1,17 @@
+import classnames from "classnames";
+import { BASE_LEFT_PADDING_MINUS, MOBILE_WIDTH } from "../../constants";
+import { isMobile } from "react-device-detect";
+
 export const Br = () => {
   return (
-    <div className="py-1 h-1">
-      <div className="absolute w-full h-1 bg-[#D4D4D4]"></div>
+    <div className="py-1 h-[1px] relative">
+      <span
+        className={classnames(
+          "absolute h-[1px] bg-[#D4D4D4]",
+          BASE_LEFT_PADDING_MINUS,
+          !isMobile ? `${MOBILE_WIDTH}` : "w-[calc(100%+3rem)]"
+        )}
+      ></span>
     </div>
   );
 };
