f3e152793b9d297e4a6046928b4b75d0111f676e
loopy-lim <loopy@jnu.ac.kr>
2023-09-27 14:06:31 +0900

    feat: add iOS notch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import { router } from "../router/router";
-import { isMobile } from "react-device-detect";
+import { isIOS, isMobile } from "react-device-detect";
 import { MOBILE_WIDTH } from "./constants";
 
 const queryClient = new QueryClient();
@@ -10,7 +10,8 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={isMobile ? "" : MOBILE_WIDTH}>
+      {isIOS && <div className="notch-ios"></div>}
+      <div className={!isMobile && MOBILE_WIDTH}>
         <RouterProvider router={router} />
       </div>
       <ReactQueryDevtools initialIsOpen={false} />
