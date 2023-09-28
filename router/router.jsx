a5adefe7a9e6abe5bd343ec5fd5d1cdc5dba693e
loopy-lim <loopy@jnu.ac.kr>
2023-09-28 17:40:33 +0900

    feat: add product
    - add product company
    - add product detail
    - add product rental button
    - add product page
    - change carousel to common
    - add className at br in common

--- router/router.jsx
+++ router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { TestPage } from "../src/pages/Test.page";
-import { HomePage } from "../src/pages/home.page";
+import { HomePage } from "../src/pages/Home.page";
+import { ProductDetailPage } from "../src/pages/ProductDetail.page";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
+  },
+  {
+    path: "/product/detail/:id",
+    element: <ProductDetailPage />,
   },
 ]);
 
