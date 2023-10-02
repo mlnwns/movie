c597535b5370b88c3a149bafb206a7250e83523a
minkonging <skysmksky@gmail.com>
2023-10-02 20:45:10 +0900

    feat: 출금 및 충전 버튼 색 바꿈

--- src/components/paymoney/MyMoney.jsx
+++ src/components/paymoney/MyMoney.jsx
@@ -24,7 +24,7 @@ export const MyMoney = () => {
         <>
             <div className="bg-[#F1F1F1] p-2 m-6 rounded-lg w-[350px] h-[80px]" style={{ display: 'flex', justifyContent: 'center' }}>
                 <Txt typography="h2" className="m-2 p-1">{piece}원</Txt>
-                <Button size="small" className="w-1/5 m-2" onClick={() => setIsModalAlertOpen((prev) => !prev)}>출금</Button>
+                <Button size="small" className="w-1/5 m-2 bg-gray-500 border-gray-500" onClick={() => setIsModalAlertOpen((prev) => !prev)}>출금</Button>
                 <Modal.Alert
                     isOpen={isModalAlertOpen}
                     onClose={() => {
