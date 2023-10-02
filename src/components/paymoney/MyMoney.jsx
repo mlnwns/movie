7ee3c4d4f049865563a6b40bbcfaf98568a878bc
minkonging <skysmksky@gmail.com>
2023-10-02 19:33:48 +0900

    feat: 페이머니 출금 modal창 생성 완료

--- src/components/paymoney/MyMoney.jsx
+++ src/components/paymoney/MyMoney.jsx
@@ -1,27 +1,21 @@
+import { useState } from "react";
 import { Navigate } from "react-router-dom";
 import { Br } from "../common/Br.component";
 import { Button } from "../common/Button.component";
 import { Txt } from "../common/Txt.component";
+import { Modal } from "../common/Modal.component";
 
 export const MyMoney = () => {
+    const [isModalAlertOpen, setIsModalAlertOpen] = useState(false);
+
     const {
         piece,
     } = {
         piece: "20000",
     };
-    const handleButtonClick1 = () => {
-        <Modal.Alert
-            isOpen={isModalAlertOpen}
-            onClose={() => {
-                setIsModalAlertOpen(false);
-            }}
-            onRequestClose={() => setIsModalAlertOpen(false)}
-        >
-            boromi@gmail.com로 예금주명, 계좌번호, 은행명, 출금 금액에 관한 내용을 전달 해 주시기 바랍니다 :&rpar;
-        </Modal.Alert>
-    };
 
-    const handleButtonClick2 = () => {
+
+    const handleButtonClick = () => {
         Navigate("/payment/charge");
     };
 
@@ -29,8 +23,25 @@ export const MyMoney = () => {
         <>
             <div className="bg-FCFCFE p-4 rounded-lg">
                 <Txt typography="h5">{piece}원</Txt>
-                <Button onClick={handleButtonClick1}>출금</Button>
-                <Button onClick={handleButtonClick2}>충전</Button>
+                <Button onClick={() => setIsModalAlertOpen((prev) => !prev)}>출금</Button>
+                <Modal.Alert
+                    isOpen={isModalAlertOpen}
+                    onClose={() => {
+                        setIsModalAlertOpen(false);
+                    }}
+                    onRequestClose={() => setIsModalAlertOpen(false)}
+                >
+                    boromi@gmail.com로
+                    <br />
+                    예금주명, 계좌번호, 은행명,
+                    <br />
+                    출금 금액에 관한 내용을
+                    <br />
+                    전달해 주시기 바랍니다 :)
+                    <br />
+                    <br />
+                </Modal.Alert>
+                <Button onClick={handleButtonClick}>충전</Button>
             </div>
 
         </>
