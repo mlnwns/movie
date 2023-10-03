9ab9b04e4d07736d8007286ff871eda60de7125d
minkonging <skysmksky@gmail.com>
2023-10-03 21:29:28 +0900

    feat: 페이머니 잔액 세부 ui 변경

--- src/components/paymoney/MyMoney.jsx
+++ src/components/paymoney/MyMoney.jsx
@@ -4,22 +4,19 @@ import { Br } from "../common/Br.component";
 import { Button } from "../common/Button.component";
 import { Txt } from "../common/Txt.component";
 import { Modal } from "../common/Modal.component";
+import "../paymoney/MyMoney.css";
 
 export const MyMoney = () => {
     const [isModalAlertOpen, setIsModalAlertOpen] = useState(false);
     const navigate = useNavigate();
-
     const {
         piece,
     } = {
         piece: "20000",
     };
-
-
     const handleButtonClick = () => {
         navigate('/paymoney/charge');
     };
-
     return (
         <>
             <div className="bg-[#F1F1F1] p-2 m-6 rounded-lg w-[400px] h-[80px] flex justify-center">
@@ -32,21 +29,15 @@ export const MyMoney = () => {
                     }}
                     onRequestClose={() => setIsModalAlertOpen(false)}
                 >
-                    boromi@gmail.com로
-                    <br />
-                    예금주명, 계좌번호, 은행명,
-                    <br />
-                    출금 금액에 관한 내용을
-                    <br />
-                    전달해 주시기 바랍니다 :)
-                    <br />
-                    <br />
+                    <div className="modalContent">
+                        boromi@gmail.com로<br />
+                        예금주명, 계좌번호, 은행명,<br />
+                        출금 금액에 관한 내용을<br />
+                        전달해 주시기 바랍니다 :)<br />
+                    </div>
                 </Modal.Alert>
                 <Button size="small" className="w-1/4 m-2" onClick={handleButtonClick}>충전</Button>
-
             </div>
-
         </>
-    )
-
-}
+    );
+};
