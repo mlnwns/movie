00ff5bc949c17200e5e57176510f0f6773db94f9
minkonging <skysmksky@gmail.com>
2023-10-03 16:41:12 +0900

    feat: MyMoney css->tailwind 피드백 수정

--- src/components/paymoney/MyMoney.jsx
+++ src/components/paymoney/MyMoney.jsx
@@ -22,7 +22,7 @@ export const MyMoney = () => {
 
     return (
         <>
-            <div className="bg-[#F1F1F1] p-2 m-6 rounded-lg w-[350px] h-[80px]" style={{ display: 'flex', justifyContent: 'center' }}>
+            <div className="bg-[#F1F1F1] p-2 m-6 rounded-lg w-[350px] h-[80px] flex justify-center">
                 <Txt typography="h2" className="m-2 p-1">{piece}원</Txt>
                 <Button size="small" className="w-1/5 m-2 bg-gray-500 border-gray-500" onClick={() => setIsModalAlertOpen((prev) => !prev)}>출금</Button>
                 <Modal.Alert
